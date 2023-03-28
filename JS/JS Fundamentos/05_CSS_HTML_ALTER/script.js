31071b377046ddc606e1cd4a8f381f557ab536b8
DanyProgramICCP <dany.hernandez@edurenca.cl>
2023-03-28 14:13:44 -0300

    ok

--- JS/JS Fundamentos/05_CSS_HTML_ALTER/script.js
+++ JS/JS Fundamentos/05_CSS_HTML_ALTER/script.js
@@ -1,19 +1,29 @@
+var saiyaImg = document.querySelector("#fav-saiya");
 
+console.log(saiyaImg);
+
+function callVegeta(element){
+    saiyaImg.src = "vegeta1.png";
+    element.style.backgroundColor = "goldenrod";
+    element.remove();
+   // console.log(saiyaImg.src);
+  //  console.log(element);
+}
+
+function callGoku(element){
+    element.classList.add("btn");
+    saiyaImg.src = "goku1.png";
+    console.log(element.classList);
+   
+}
+
+function setActive(element) {
+    console.log(element.classList);
+    if(element.innerText = "Cambiar al modo oscuro") {
+        element.innerText = "Cambiar al modo claro";
+       // element.classList.remove("dark-mode");
+    } else{
+        element.innerText = "Cambiar al modo claro";
+    }
     
-    function setActive(element) {
-        element.classList.add("dark-mode");
-    }
-
-    function setActive(element) {
-        element.style.backgroundColor = "#222222";
-        element.style.color = "#ffffff";
-    }
-    function setActive(element) {
-        if(element.classList.include("dark-mode")) {
-            element.innerText = "Cambiar al modo claro";
-            element.classList.remove("dark-mode");
-        } else {
-            element.innerText = "Cambiar al modo oscuro";
-            element.classList.add("dark-mode");
-        }
-    }
+}
