3364e54396c6610ea454f41149bbe0b90742472a
DanyProgramICCP <dany.hernandez@edurenca.cl>
2023-10-17 12:43:43 -0300

    ok

--- Python/1- funciones comunes/fundamentals/11-8 Herencia Multiple/test_figura_geometrica.py
+++ Python/1- funciones comunes/fundamentals/11-8 Herencia Multiple/test_figura_geometrica.py
@@ -5,4 +5,6 @@ print(cuadrado1.ancho)
 print(cuadrado1.alto)
 print(cuadrado1.color)
 
-print(cuadrado1.calcularArea())
+print(f'Calculo del Area del Cuadrado: {cuadrado1.calcularArea()}')
+
+print(Cuadrado.mro())
